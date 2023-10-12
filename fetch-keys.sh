af74712f6c5e569ad7acb6b0617920066d89ec65
Niall Daley <niall@shatteredsilicon.net>
2023-10-12 22:54:31 +0100

    Initial commit

---
+++ fetch-keys.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+(
+echo "##### Unlocking ZFS encrypted volumes"
+
+while read -u 3 ds enc key
+do
+	if [ "${enc}" != "off" -a "${key}" != "-" ]
+	then
+		echo "Loading key for ${ds}"
+		if (echo -n "${key}" | clevis decrypt | zfs load-key -L prompt "${ds}")
+		then
+			echo "Dataset ${ds} unlocked"
+		else
+			echo "FAILED TO UNLOCK ${ds}"
+		fi
+	fi
+done 3< <(zfs list -H -o name,encryption,zfs-tpm:key)
+) 1>&2
---
+++ module-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+check() {
+	return 0
+}
+
+depends() {
+	echo zfs
+	return 0
+}
+
+install() {
+	inst_multiple $(rpm -ql --noartifact clevis-pin-tpm2 clevis jose tpm2-tss | grep -v '^/usr/share')
+
+	# Install our hook before the standard ZFS one to unlock what we can
+	inst_hook pre-mount 85 "${moddir}/fetch-keys.sh"
+}
